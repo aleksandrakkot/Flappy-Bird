9cd866e1ebf57e76ae7e8a479d6916f445e46992
Aleksandra Kot <aleksandrak.kot@gmail.com>
2022-01-06 18:53:25 +0100

    version 1.0

---
+++ src/Dimensions.java
@@ -0,0 +1,3 @@
+public class Dimensions {
+    public static int SIZE = 800, MAPMAXHEIGHT = 700, MAPMINHEIGHT = 0,PLAYERSIZE = 30;
+}
---
+++ src/FlappyCat.java
@@ -0,0 +1,54 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class FlappyCat implements ActionListener, MouseListener, KeyListener{
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
