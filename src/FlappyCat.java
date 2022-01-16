e70a7208c3fb9b36273f23559ad5af7465f8c70c
Aleksandra Kot <aleksandrak.kot@gmail.com>
2022-01-16 19:24:20 +0100

    16.01

--- src/FlappyCat.java
+++ src/FlappyCat.java
@@ -25,14 +25,100 @@ public class FlappyCat implements ActionListener, MouseListener, KeyListener{
     }
 
     public void initialize() {
+        player = new Rectangle(Dimensions.SIZE / 2 - 10, Dimensions.SIZE / 2 - 10, Dimensions.PLAYERSIZE, Dimensions.PLAYERSIZE);
+        pipes = new ArrayList<>();
+        for(int i = 0; i < 4; i++){
+            addPipe(true);
+        }
+    }
+
+    public void addPipe(boolean start){
+        int space = 350;
+        int width = 100;
+        int height = 50 + rand.nextInt(350);
+
+        if (start){
+            pipes.add(new Rectangle(Dimensions.SIZE + width + pipes.size() * 300, Dimensions.MAPMAXHEIGHT - height, width, height));
+            pipes.add(new Rectangle(Dimensions.SIZE + width + (pipes.size() - 1) * 300, 0, width, Dimensions.SIZE - height - space));
+        }
+        else{
+            pipes.add(new Rectangle(pipes.get(pipes.size() - 1).x + 600, Dimensions.MAPMAXHEIGHT - height, width, height));
+            pipes.add(new Rectangle(pipes.get(pipes.size() - 1).x, 0, width, Dimensions.SIZE - height - space));
+        }
     }
 
     public void gameSettings() {
+        renderer = new Renderer();
+        rand = new Random();
+        jframe.add(renderer);
+        jframe.setTitle("Flappy Creep");
+        jframe.setIconImage(visual.cat);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setSize(Dimensions.SIZE, Dimensions.SIZE);
+        jframe.addMouseListener(this);
+        jframe.addKeyListener(this);
+        jframe.setResizable(false);
+        jframe.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        int speed = 10;
 
+        timerTick++;
+
+        if (started){
+            for (Rectangle pipe : pipes) {
+                pipe.x -= speed;
+            }
+
+            if (timerTick % 2 == 0 && yMove < 15){
+                yMove += 2;
+            }
+
+            for (int i = 0; i < pipes.size(); i++){
+                Rectangle pipe = pipes.get(i);
+
+                if (pipe.x + pipe.width < 0){
+                    pipes.remove(pipe);
+
+                    if (pipe.y == 0){
+                        addPipe(false);
+                    }
+                }
+            }
+
+            player.y += yMove;
+
+            for (Rectangle pipe : pipes){
+                if (pipe.y == 0 && player.x + player.width / 2 > pipe.x + pipe.width / 2 - 7 && player.x + player.width / 2 < pipe.x + pipe.width / 2 + 7 && !gameOver){
+                    score++;
+                }
+
+                if (pipe.intersects(player)){
+                    gameOver = true;
+
+                    if (player.x <= pipe.x){
+                        player.x = pipe.x - player.width;
+                    }
+                    else if (pipe.y != 0){
+                        player.y = pipe.y - player.height;
+                    }
+                    else if (player.y < pipe.height){
+                        player.y = pipe.height;
+                    }
+                }
+                else if (player.y > Dimensions.MAPMAXHEIGHT || player.y < 0){
+                    gameOver = true;
+                }
+                else if (player.y + yMove >= Dimensions.MAPMAXHEIGHT){
+                    player.y = Dimensions.MAPMAXHEIGHT - player.height;
+                    gameOver = true;
+                }
+                finalScore = score;
+            }
+        }
+        renderer.repaint();
     }
 
     @Override
@@ -42,7 +128,14 @@ public class FlappyCat implements ActionListener, MouseListener, KeyListener{
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_SPACE && !begining){
+            jump();
+        }
 
+        if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
+        {
+            System.exit(0);
+        }
     }
 
     @Override
@@ -52,7 +145,48 @@ public class FlappyCat implements ActionListener, MouseListener, KeyListener{
 
     @Override
     public void mouseClicked(MouseEvent e) {
+        if(e.getButton()==1) {
 
+
+            Point coords = e.getPoint();
+            if (gameOver && coords.x < 80 && coords.y < 90) {
+                begining = true;
+                started = false;
+                gameOver = false;
+                initialize();
+            }
+            if (!begining) {
+                jump();
+            } else if (coords.x < 650 && coords.x > 150 && coords.y > 430 && coords.y < 490) {
+                begining = false;
+            } else if (coords.x < 650 && coords.x > 420 && coords.y > 540 && coords.y < 590) {
+                System.exit(0);
+            }
+        }
+    }
+
+    public void jump(){
+        if (gameOver){
+            player = new Rectangle(Dimensions.SIZE / 2 - 10, Dimensions.SIZE / 2 - 10, Dimensions.PLAYERSIZE, Dimensions.PLAYERSIZE);
+            pipes.clear();
+            yMove = 0;
+            score = 0;
+
+            for(int i = 0; i < 4; i++){
+                addPipe(true);
+            }
+            gameOver = false;
+        }
+
+        if (!started){
+            started = true;
+        }
+        else {
+            if (yMove > 0){
+                yMove = 0;
+            }
+            yMove -= 10;
+        }
     }
 
     @Override
@@ -76,5 +210,29 @@ public class FlappyCat implements ActionListener, MouseListener, KeyListener{
     }
 
     public void refresh(Graphics g) {
+        if (begining) {
+            visual.homeScreen(g);
+        }
+        else {
+            visual.drawGame(g, player);
+            for (Rectangle pipe : pipes) {
+                visual.drawPipe(g, pipe);
+            }
+            g.setColor(Color.white);
+
+            if (!started) {
+                visual.startBanner(g);
+            }
+            if (gameOver) {
+                visual.gameOverScreen(g, finalScore);
+            }
+            if (!gameOver && started) {
+                visual.scoreBar(g, score);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        flappyCat = new FlappyCat();
     }
 }
--- src/Visuals.java
+++ src/Visuals.java
@@ -6,20 +6,74 @@ import java.io.IOException;
 public class Visuals {
 
     public Font font;
-    public Image cat, back, button, small_button, menu_background;
+    public Image cat, back, button, small_button, menu_background, pipe;
     private Dimensions Constants;
 
+    public Visuals()
+    {
+        try {
+            font = Font.createFont(Font.TRUETYPE_FONT, new File(".\\src\\font\\font.otf")).deriveFont(60f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(".\\src\\font\\font.otf")));
+        } catch (IOException | FontFormatException e) {
+            System.exit(1);
+        }
 
-
-    public void showScore(Graphics g, int score){
-        g.setFont(font);
-        g.drawString("Score", 25, 710);
-        g.setFont(font);
-        g.drawString(String.valueOf(score), 65, 750);
+        menu_background = new ImageIcon(".\\src\\img\\menu-background.png").getImage();
+        button = new ImageIcon(".\\src\\img\\button.png").getImage();
+        small_button = new ImageIcon(".\\src\\img\\small_button.png").getImage();
+        back = new ImageIcon(".\\src\\img\\back.png").getImage();
+        cat = new ImageIcon(".\\src\\img\\cat.png").getImage();
+        pipe = new ImageIcon(".\\src\\img\\cat.png").getImage();
     }
 
-    public void startGame(Graphics g){
+    public void homeScreen(Graphics g) {
+        g.drawImage(menu_background, 0, 0, Constants.SIZE, Constants.SIZE, null);
+
+        g.setColor(Color.GRAY);
+        g.setFont(font);
+
+        g.drawString("Flappy Cat!", 100, Constants.SIZE / 2 - 150);
+
+        g.setColor(Color.WHITE);
+
+
+        g.drawImage(button, 150, 400, 500, 60, null);
+        g.drawString("Start Game", 300, 444);
+
+        g.drawImage(small_button, 410, 500, 240, 60, null);
+        g.drawString("Quit", 500, 544);
+    }
+
+    public void drawGame(Graphics g, Rectangle player) {
+        g.drawImage(back, 0, 0, Constants.SIZE, Constants.MAPMAXHEIGHT, null);
+        //g.drawImage(gameGround, 0, Constants.MAPMAXHEIGHT, Constants.SIZE, Constants.SIZE - Constants.MAPMAXHEIGHT, null);
+        g.drawImage(cat, player.x, player.y, player.width, player.height, null);
+    }
+
+    public void drawPipe(Graphics g, Rectangle pipeRect) {
+        g.setColor(Color.black.darker());
+        g.drawImage(pipe, pipeRect.x, pipeRect.y, pipeRect.width, pipeRect.height, Color.BLACK,null);
+    }
+
+    public void startBanner(Graphics g) {
         g.setFont(font);
         g.drawString("Click to start!", 75, Constants.SIZE / 2 - 50);
+    }
+
+    public void gameOverScreen(Graphics g, int finalScore) {
+        int tooLong;
+        g.setFont(font);
+        g.drawString("Game Over!", 300, Constants.SIZE / 2 - 150);
+        if(finalScore <= 10) tooLong = 0;  else tooLong = 5;
+        g.drawString("Your score: " + finalScore, 270 - tooLong, Constants.SIZE / 2 - 50);
+        g.drawString("Click to play again",240,Constants.SIZE / 2 + 50);
+        g.drawImage(cat,0, 0, 75, 65, null);
+    }
+
+    public void scoreBar(Graphics g, int score) {
+        g.setFont(font);
+        g.setColor(Color.red);
+        g.drawString("Score: " + score, 25, 730);
     }
 }
